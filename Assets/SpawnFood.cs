c8f581d4b2b65905b3834e382ef9bce415c0ba7b
Emil Skogsund <skogix@gmail.com>
2020-01-29 09:43:12 +0100

    Init test

--- Assets/SpawnFood.cs
+++ Assets/SpawnFood.cs
@@ -10,18 +10,18 @@ public class SpawnFood : MonoBehaviour
   public Transform borderBottom;
   public Transform borderLeft;
   public Transform borderRight;
-    // Start is called before the first frame update
   void Start()
   {
     InvokeRepeating("Spawn", 3, 4);
   }
   void Spawn()
   {
+    //sätt random mellan borderpositions
     int x = (int)Random.Range(
       borderLeft.position.x, borderRight.position.x);
     int y = (int)Random.Range(
       borderTop.position.y, borderBottom.position.y);
-
+    //spawna foodprefab 
     Instantiate(foodPrefab,
       new Vector2(x, y),
       Quaternion.identity);
